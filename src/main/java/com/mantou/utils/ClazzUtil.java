ea3ee98dde945d3f03d8269106f03d9ef93653db
mantou <mt15027896686@163.com>
2021-11-04 18:22:46 +0800

    返回数据改成LinkedHashMap类型

--- src/main/java/com/mantou/utils/ClazzUtil.java
+++ src/main/java/com/mantou/utils/ClazzUtil.java
@@ -1,17 +1,21 @@
 package com.mantou.utils;
 
 import java.lang.reflect.Field;
+import java.util.LinkedHashMap;
 
 /**
- * 利用反射获取数据
+ * @author mantou
+ * @date 2021/10/29 22:24
+ * @return String
+ * @desc 利用反射获取原报文数据
  */
 public class ClazzUtil {
 
-    public static String getData(Object obj){
-
+    public static LinkedHashMap<String,Object> getData(Object obj){
+        LinkedHashMap<String,Object> map = new LinkedHashMap<>();
         Class clazz = obj.getClass();
         Field[] fields = clazz.getDeclaredFields();
-        String nameVlues="";
+        //String nameVlues="";
         for (Field field : fields) {
             //打开私有访问
             field.setAccessible(true);
@@ -22,8 +26,9 @@ public class ClazzUtil {
                 Object value;
                 try {
                     value = field.get(obj);
+                    map.put(name,value);
                     //一个个赋值
-                    nameVlues += field.getName()+":"+value+",";
+                   // nameVlues += field.getName()+":"+value+",";
                 } catch (IllegalAccessException e) {
                     e.printStackTrace();
                 }
@@ -31,11 +36,11 @@ public class ClazzUtil {
         }
 
         //获取最后一个逗号的位置
-        int lastIndex = nameVlues.lastIndexOf(",");
+        //int lastIndex = nameVlues.lastIndexOf(",");
         //不要最后一个逗号","
-        String  data= nameVlues.substring(0,lastIndex) ;
+        //String  data= nameVlues.substring(0,lastIndex) ;
 
-        return data;
+        return map;
     }
 
 }
